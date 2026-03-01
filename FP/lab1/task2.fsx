0afcb2148f41c407d2cf59588924a7d36d41513c
Maxim Grigorev <maxkraft06@gmail.com>
2026-03-01 20:30:37 +0300

    remove extra parentheses 3

--- FP/lab1/task2.fsx
+++ FP/lab1/task2.fsx
@@ -36,27 +36,27 @@ let newton f f' x0 eps maxIter =
 
 // 5: sqrt(1 - x) - tg(x) = 0, [0, 1]
 let f1 x = sqrt (1.0 - x) - tan x
-let f1' x = -1.0 / (2.0 * sqrt (1.0 - x)) - 1.0 / ((cos x) * (cos x))
+let f1' x = -1.0 / (2.0 * sqrt (1.0 - x)) - 1.0 / (cos x * cos x)
 let phi1 x = atan (sqrt (1.0 - x))
 let a1, b1, x01, table1 = 0.0, 1.0, 0.5, 0.5768
 
 // 6: x + cos(x^0.52 + 2) = 0, [0.5, 1]
 let f2 x = x + cos (x ** 0.52 + 2.0)
-let f2' x = 1.0 - sin (x ** 0.52 + 2.0) * 0.52 * (x ** -0.48)
+let f2' x = 1.0 - sin (x ** 0.52 + 2.0) * 0.52 * x ** -0.48
 let phi2 x = -cos (x ** 0.52 + 2.0)
 let a2, b2, x02, table2 = 0.5, 1.0, 0.75, 0.9892
 
 // 7: 3*ln^2(x) + 6*ln(x) - 5 = 0, [1, 3]
-let f3 x = 3.0 * (log x) ** 2.0 + 6.0 * log x - 5.0
+let f3 x = 3.0 * log x ** 2.0 + 6.0 * log x - 5.0
 let f3' x = (6.0 * log x + 6.0) / x
-let phi3 x = exp ((5.0 - 3.0 * (log x) ** 2.0) / 6.0)
+let phi3 x = exp ((5.0 - 3.0 * log x ** 2.0) / 6.0)
 let a3, b3, x03, table3 = 1.0, 3.0, 2.0, 1.8832
 
 let solveOne f f' phi a b x0 =
     let dRoot, dIters = dichotomy f a b eps maxIterations
     let iRoot, iIters = iterations phi x0 eps maxIterations
     let nRoot, nIters = newton f f' x0 eps maxIterations
-    (dRoot, dIters, iRoot, iIters, nRoot, nIters)
+    dRoot, dIters, iRoot, iIters, nRoot, nIters
 
 let s1 = solveOne f1 f1' phi1 a1 b1 x01
 let s2 = solveOne f2 f2' phi2 a2 b2 x02
